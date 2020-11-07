377caf75033f566e2a79b3f9086be45dd8691e64
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-11-07 13:57:00 +0800

    end of project

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -6,11 +6,19 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
     
     @IBAction func close() {
